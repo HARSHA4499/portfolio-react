0f1b915a12a4acef90967481fba4c1cc18d84a99
HARSHA4499 <shvrp4499@gmail.com>
2023-11-25 16:42:19 -0600

    commit

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,8 +2,7 @@ import Proj1 from "../assets/proj1.png";
 import Proj2 from "../assets/proj2.jpeg";
 import Proj3 from "../assets/proj3.avif";
 import Proj4 from "../assets/proj4.jpeg";
-import Proj5 from "../assets/proj4.jpeg";
-import Proj6 from "../assets/proj4.jpeg";
+
 
 export const ProjectList = [
   {
@@ -26,14 +25,5 @@ export const ProjectList = [
     image: Proj4,
     skills: "Python Programming",
   },
-  {
-    name: "Dashboard Visualizer",
-    image: Proj5,
-    skills: "JavaScript,HTML,CSS",
-  },
-  {
-    name: "Mobile Game",
-    image: Proj6,
-    skills: "React Native,JavaScript,HTML,CSS",
-  },
+
 ];
