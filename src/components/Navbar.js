55748ef4c6030f42fbeac36ab568fd958d6c280a
harsha4499 <shvrp4499@gmail.com>
2024-09-16 13:04:31 -0500

    qq

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,33 @@
-import React, { useState,useEffect} from 'react'
-import {Link,useLocation} from 'react-router-dom'
-import "../styles/Navbar.css"
-import ReorderIcon from '@mui/icons-material/Reorder';
-
-
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
 function Navbar() {
-    const [expandNavbar,setExpandNavbar]=useState(false);
+  const [expandNavbar, setExpandNavbar] = useState(false);
 
-    const location=useLocation();
+  const location = useLocation();
 
-    useEffect(()=>{
-        setExpandNavbar(false);
-    },[location]);
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
 
   return (
-  <div className='navbar' id={expandNavbar? "open":"close"}>
-    <div className='toggleButton'>
-        <button onClick={()=>setExpandNavbar((prev)=>!prev)}>
-            <ReorderIcon />
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button onClick={() => setExpandNavbar((prev) => !prev)}>
+          <ReorderIcon />
         </button>
-    </div>
-    <div className='links'>
-        <Link to="/portfolio">Home</Link>
+      </div>
+      <div className="links">
+        <Link to="/">Home</Link>
         <Link to="./projects">Projects</Link>
-        <Link id="experience" to="./experience">Experience</Link>
+        <Link id="experience" to="./experience">
+          Experience
+        </Link>
+      </div>
     </div>
-  </div>
   );
 }
 
-export default Navbar
+export default Navbar;
